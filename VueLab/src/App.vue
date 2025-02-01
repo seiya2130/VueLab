258392ee64aa35c0abb15691b8b32a68af660b42
SeiyaSatoh <satohseiya0312@gmail.com>
2025-02-01 18:47:21 +0900

    feat: flex検証

--- VueLab/src/App.vue
+++ VueLab/src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <ClipBoard />
+  <div class="parent">
+    <div class="child">aaaaaaaaaaaaaa</div>
+    <div>b</div>
+    <div>c</div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Parent from './components/Parent.vue';
 import ClipBoard from './components/ClipBoard.vue';
 
 </script>
+<style
+  scoped
+  lang="scss"
+>
+  .parent {
+    display: flex;
+    width: 100px;
+  }
+  .child {
+   min-width: 0;
+   overflow-wrap: break-word;
+  }
+</style>
