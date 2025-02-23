bc8e34e16ed24ae9c0f14d4c57f23baf80063d53
SeiyaSatoh <satohseiya0312@gmail.com>
2025-02-23 18:00:55 +0900

    feat: format追加

--- VueLab/src/App.vue
+++ VueLab/src/App.vue
@@ -6,4 +6,16 @@
 import Parent from './components/Parent.vue';
 import ClipBoard from './components/ClipBoard.vue';
 
+
+const formatDate = (date: Date): string => {
+  const year = date.getFullYear();
+  // getMonth() は 0〜11 なので、+1 して正しい月に変換
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+const date = new Date();
+console.log(formatDate(date));
+
 </script>
